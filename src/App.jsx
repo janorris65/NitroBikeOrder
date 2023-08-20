cab768bf9bf04bf740de733e4e3cce25c167bc25
Joshua <janorris65@gmail.com>
2023-08-19 22:49:59 -0500

    split packages into components, create accordion

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Total from "./components/Total";
 import NavBar from "./components/NavBar";
 import Packages from "./components/Packages";
+import PackComponent from "./components/PackComponent";
+import ChocolateMilk from "../assets/IMG_5845.jpg";
 
 function App() {
   const [boxState, setboxState] = useState([]);
@@ -24,38 +26,71 @@ function App() {
   const DClick = (event) => {
     setdState(event);
   };
-  const [hourState, sethourState] = useState([]);
-  const HourClick = (event) => {
-    sethourState(event);
-  };
+
   return (
     <>
       <NavBar />
       <div className="flex-col bg-gradient-to-r from-gray-300 to-emerald-600 font-serif">
-        <Packages
-          OnBox={BoxClick}
-          boxtotal={boxState}
-          OnOut={OutClick}
-          outtotal={outState}
-          OnDeck={DeckClick}
-          decktotal={deckState}
-          OnStar={StarClick}
-          startotal={starState}
-          OnD={DClick}
-          dtotal={dState}
-          OnHour={HourClick}
-          hourtotal={hourState}
-        />
         <Total
           total={
             Number(boxState) +
             Number(outState) +
             Number(deckState) +
             Number(starState) +
-            Number(dState) +
-            Number(hourState)
+            Number(dState)
           }
         />
+        <div className="m-4 mx-auto max-w-md overflow-hidden rounded border-4 border-gray-600 bg-emerald-300 shadow-2xl">
+          <h2>Packages</h2>
+          <PackComponent
+            OnPack={BoxClick}
+            packtotal={boxState}
+            Starbuckspic={ChocolateMilk}
+            PriceA={529.63}
+            PriceB={1059.15}
+            title="Box Seats"
+            description="Enjoy the best of the experience. Rich Nitro Cold Brew, Cold
+            Frappuccinos, and Refreshing Lemonades"
+          />
+          <PackComponent
+            OnPack={OutClick}
+            packtotal={outState}
+            Starbuckspic={ChocolateMilk}
+            PriceA={511.58}
+            PriceB={1005.02}
+            title="Outfield"
+            description="Enjoy the excitement to front row big events with Iced Cold Brew,
+            Frappuccinos, and Select Lemonades"
+          />
+          <PackComponent
+            OnPack={DeckClick}
+            packtotal={deckState}
+            Starbuckspic={ChocolateMilk}
+            PriceA={481.67}
+            PriceB={846.12}
+            title="Upper Deck"
+            description="Take it all in! Enjoy Rich Iced Cold Brew and Sweet Lemonades"
+          />
+          <PackComponent
+            OnPack={StarClick}
+            packtotal={starState}
+            Starbuckspic={ChocolateMilk}
+            PriceA={444.78}
+            PriceB={802.99}
+            title="All Star Break"
+            description="Sit back and relax! Enjoy Refreshing Lemonades and Cold Frappuccinos"
+          />
+          <PackComponent
+            OnPack={DClick}
+            packtotal={dState}
+            Starbuckspic={ChocolateMilk}
+            PriceA={356.58}
+            PriceB={580.75}
+            title="Designated Hitter"
+            description="Classic Clutch Performance. Rich Nitro Cold Brew and Refreshing
+            Lemonades"
+          />
+        </div>
       </div>
     </>
   );
---
+++ src/components/PackComponent.jsx
@@ -0,0 +1,158 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+export default function PackComponent({
+  Starbuckspic,
+  packtotal,
+  OnPack,
+  PriceA,
+  PriceB,
+  title,
+  description,
+}) {
+  const [click, setClick] = useState(true);
+  const onClicks = (event) => {
+    event.preventDefault();
+    setClick((prev) => !prev);
+  };
+  const onChanges = (event) => {
+    console.log(
+      event.target.parentNode.parentNode.querySelector("#BoxTwentythirty")
+        .checked,
+      event.target.parentNode.parentNode.querySelector("#BoxSixtyeighty")
+        .checked,
+      event.target.parentNode.parentNode.querySelector("#numBoxTwenty")?.value,
+      event.target.parentNode.parentNode.querySelector("#numBoxSixty")?.value
+    );
+
+    console.log(packtotal);
+    // event.preventDefault();
+    if (
+      event.target.parentNode.parentNode.querySelector("#BoxTwentythirty")
+        .checked &&
+      !event.target.parentNode.parentNode.querySelector("#BoxSixtyeighty")
+        .checked
+    ) {
+      OnPack(
+        Number(
+          event.target.parentNode.parentNode.querySelector("#numBoxTwenty")
+            ?.value * PriceA
+        )
+      );
+    } else if (
+      !event.target.parentNode.parentNode.querySelector("#BoxTwentythirty")
+        .checked &&
+      event.target.parentNode.parentNode.querySelector("#BoxSixtyeighty")
+        .checked
+    ) {
+      OnPack(
+        Number(
+          event.target.parentNode.parentNode.querySelector("#numBoxSixty")
+            ?.value * PriceB
+        )
+      );
+    } else if (
+      event.target.parentNode.parentNode.querySelector("#BoxTwentythirty")
+        .checked &&
+      event.target.parentNode.parentNode.querySelector("#BoxSixtyeighty")
+        .checked
+    ) {
+      OnPack(
+        Number(
+          event.target.parentNode.parentNode.querySelector("#numBoxTwenty")
+            ?.value * PriceA
+        ) +
+          Number(
+            event.target.parentNode.parentNode.querySelector("#numBoxSixty")
+              ?.value * PriceB
+          )
+      );
+    } else {
+      OnPack(0);
+    }
+  };
+  return (
+    <div>
+      <form onChange={onChanges}>
+        <div
+          className="group m-2 mx-auto flex max-w-sm flex-col overflow-hidden rounded-lg bg-stone-200 p-2 text-center
+          shadow-2xl"
+        >
+          <button
+            onClick={onClicks}
+            className="rounded-full bg-emerald-500 px-4 py-2 font-bold text-white hover:bg-emerald-400"
+          >
+            <h3>
+              {title} <span>{click ? "+" : "-"}</span>
+            </h3>
+          </button>
+          <img
+            src={Starbuckspic}
+            className="h-[200px] rounded-lg object-cover p-1"
+            alt="Pic of Nitros, Fraps, Lemonades"
+          ></img>
+          <p>{description}</p>
+          <div hidden={click}>
+            <div className="flex  justify-evenly">
+              <input
+                className="m-1 self-center rounded-lg"
+                type="checkbox"
+                id="BoxTwentythirty"
+                name="BoxTwentythirty"
+                value="BoxTwentythirty"
+              />
+              <label className=" self-center" htmlFor="BoxTwentythirty">
+                20-30
+              </label>
+
+              <input
+                className="m-1 self-center rounded-lg"
+                type="text"
+                id="numBoxTwenty"
+                name="numBoxTwenty"
+                placeholder="#"
+              />
+              <label className=" self-center" htmlFor="numBoxTwenty">
+                # Packages
+              </label>
+            </div>
+            <div className="flex  justify-evenly">
+              <input
+                className="m-1 self-center rounded-lg"
+                type="checkbox"
+                id="BoxSixtyeighty"
+                name="BoxSixtyeighty"
+                value="BoxSixtyeighty"
+              />
+              <label className=" self-center" htmlFor="BoxSixtyeighty">
+                60-80
+              </label>
+
+              <input
+                className="m-1 self-center rounded-lg"
+                type="text"
+                id="numBoxSixty"
+                name="numBoxSixty"
+                placeholder="#"
+              />
+              <label className=" self-center" htmlFor="numBoxSixty">
+                # Packages
+              </label>
+            </div>
+            <div className="m-4 mx-auto max-w-sm overflow-hidden rounded bg-slate-300 shadow-2xl">
+              <h2>subtotal: {packtotal}</h2>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+PackComponent.propTypes = {
+  packtotal: PropTypes.number,
+  OnPack: PropTypes.any,
+  Starbuckspic: PropTypes.any,
+  PriceA: PropTypes.number,
+  PriceB: PropTypes.number,
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
